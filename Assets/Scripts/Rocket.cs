1d309b3ce4abc421bc0528b08389a4fe80484e32
mardt <itsmardt@gmail.com>
2025-09-10 16:42:24 +0200

    fix collision

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,18 +5,18 @@ public class Rocket : MonoBehaviour
     public float speed = 20f;
     public float lifetime = 5f;
 
+    private Rigidbody rb;
+
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Rocket collided with " + collision.gameObject.name);
+        Destroy(gameObject);
     }
 
     void OnEnable()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;        
+        rb.linearVelocity = transform.forward * speed;
         Destroy(gameObject, lifetime);
-    }
-
-    void Update()
-    {
-        transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
